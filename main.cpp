daf5747f639df40b7e3be2ad83beae3f55994dea
win4er <platon160106@gmail.com>
2023-01-21 10:42:24 +0300

    multi-file for main

--- main.cpp
+++ main.cpp
@@ -1,30 +1,6 @@
 #include <iostream>
 #include <time.h>
-
-void InsertSort(int* ar, int size) {
-    int i, j, digit;
-    for (i = 1; i < size; i++) {
-        digit = ar[i];
-        j = i - 1;
-
-        while (ar[j] < digit) {
-            if (j < 0) {
-                break;
-            }
-            *(ar + j + 1) = ar[j];
-            j--;
-        }
-        *(ar + j + 1) = digit;
-    }
-}
-
-void fillRandom(int* firstElementOfArray, int size) {
-    srand(time(nullptr));
-    for (int i = 0; i < size; i++) {
-        *(firstElementOfArray + i) = rand() % 100;
-    }
-}
-
+#include "main.hpp"
 
 int main() {
     int size = 7;
